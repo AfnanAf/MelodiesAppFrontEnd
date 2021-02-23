ee09e7512a93cae44d091cede24c633f26924961
alaa <alaa.alzahrani88@gmail.com>
2021-02-23 14:45:26 +0300

    password confirmation done @afnanaaf

--- src/user/Register.js
+++ src/user/Register.js
@@ -3,108 +3,74 @@ import { Container, Form, Button } from 'react-bootstrap'
 
 export default class Register extends Component {
     state = {
-        errors: {},
+        password: "",
+        passwordConfirm: "",
         temp: {}
     }
 
-    registerHandler = (e) => {
-        e.preventDefault();
-
-        if (this.validateInputs) {
-
-            console.log(this.state);
-
-            let temp = {};
-            temp["firstName"] = "";
-            temp["lastName"] = "";
-            temp["emailAddress"] = "";
-            temp["password"] = "";
-            temp["passwordConfirm"] = "";
-            temp["userRole"] = "";
-
-            this.setState({ temp: temp });
-
-            // alert('Demo Form is submited');
+    registerHandler = () => {
+        console.log(this.state);
+        if (this.state.password == this.state.passwordConfirm) {
             this.props.register(this.state.temp);
-
-
         } else {
-
+            alert("password doesn't match")
         }
-    }
-
-    validateInputs() {
-        let temp = this.state.temp
-        let errors = {};
-        let isValid = true;
-
-        if (!temp["firstName"]) {
-            isValid = false;
-            errors["firstName"] = "Please enter your first Name.";
-        }
-
-        if (!temp["lastName"]) {
-            isValid = false;
-            errors["lastName"] = "Please enter your last Name.";
-        }
-
-        if (!temp["emailAddress"]) {
-            isValid = false;
-            errors["emailAddress"] = "Please enter your email Address.";
-        }
-
-        if (typeof temp["emailAddress"] !== "undefined") {
-
-            var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
-            if (!pattern.test(temp["emailAddress"])) {
-                isValid = false;
-                errors["emailAddress"] = "Please enter valid email address.";
-            }
-        }
-
-        if (!temp["password"]) {
-            isValid = false;
-            errors["password"] = "Please enter your password.";
-        }
-
-        if (!temp["passwordConfirm"]) {
-            isValid = false;
-            errors["passwordConfirm"] = "Please enter your confirm password.";
-        }
-
-        if (typeof temp["password"] !== "undefined" && typeof temp["passwordConfirm"] !== "undefined") {
-
-            if (temp["password"] != temp["passwordConfirm"]) {
-                isValid = false;
-                errors["password"] = "Passwords don't match.";
-            }
-        }
-
-        this.setState({
-            errors: errors
-        });
-
-        return isValid;
     }
 
 
     changeHandler = (e) => {
-        let temp = this.state.temp
+        let temp = this.state.temp;
         temp[e.target.name] = e.target.value;
-        this.setState({ temp });
+        temp["password"] = this.state.password;
+
         console.log(temp);
+        this.setState({
+            temp
+            // password: temp["password"],
+            // passwordConfirm: temp["passwordConfirm"]
+        })
     }
 
-    // confirmPassword = (e) => {
-    //     if (e.target.value === this.state.password) {
-    //         console.log("temp password" + this.state.password);
-    //         console.log("match");
-    //     } else if (this.state.passwordConfirm === this.state.password) {
-    //         console.log("match");
-    //     } else {
-    //         console.log("Passwords don't match");
-    //     }
-    // }
+    passwordHandler = (e) => {
+        // let temp = { ... this.state }
+        // temp[e.target.name] = e.target.value;
+        // console.log(temp);
+        // this.setState({
+        //     password: temp["password"],
+        //     passwordConfirm: temp["passwordConfirm"]
+        // })
+        let temp = this.state.temp;
+        temp["password"] = e.target.value;
+        console.log(temp);
+
+        this.setState({
+            password: temp["password"],
+            // passwordConfirm: temp["passwordConfirm"]
+        })
+
+        if (e.target.value === this.state.passwordConfirm) {
+            console.log("input password" + this.state.passwordConfirm);
+            console.log("match");
+            this.changeHandler(e);
+        } else {
+            console.log("Passwords don't match");
+        }
+    }
+
+    confirmPassword = (e) => {
+        let temp = this.state.temp;
+        temp[e.target.name] = e.target.value;
+        this.setState({
+            // password: temp["password"],
+            passwordConfirm: temp["passwordConfirm"],
+        })
+        if (e.target.value === this.state.password) {
+            console.log("input password" + this.state.password);
+            console.log("match");
+        } else {
+            console.log("Passwords don't match");
+        }
+    }
 
     render() {
         return (
@@ -127,12 +93,12 @@ export default class Register extends Component {
 
                     <Form.Group>
                         <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" name="password" onChange={this.changeHandler}></Form.Control>
+                        <Form.Control type="password" name="password" onChange={this.passwordHandler}></Form.Control>
                     </Form.Group>
 
                     <Form.Group>
                         <Form.Label>Password Confirmation</Form.Label>
-                        <Form.Control type="password" name="passwordConfirm" onChange={this.changeHandler}></Form.Control>
+                        <Form.Control type="password" name="passwordConfirm" onChange={this.confirmPassword}></Form.Control>
                     </Form.Group>
 
                     <Form.Group>
