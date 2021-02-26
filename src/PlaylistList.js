fe9d180ecef6590292d5be2589ffc3e4790486a9
alaa <alaa.alzahrani88@gmail.com>
2021-02-27 02:41:11 +0300

    delete a playlist

--- src/PlaylistList.js
+++ src/PlaylistList.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import React, { Component } from "react";
 import Table from "react-bootstrap/Table";
 import { Redirect } from "react-router-dom";
@@ -6,6 +7,51 @@ import SongsPlaylist from "./SongsPlaylist";
 
 export default class PlaylistList extends Component {
 
+  state = {
+    playlists: this.props.playlists
+  }
+  deletePlaylist = (playlistId) => {
+    // e.preventDefault()
+    console.log(playlistId)
+
+
+    axios.delete("/playlist/delete?id=" + playlistId, {
+      headers: {
+        "Authorization": "Bearer " + localStorage.getItem("token")
+      }
+    })
+      .then(res => {
+        console.log("playlist deleted !");
+        console.log(res.data);
+        this.setState({
+          successMessage: "playlist is deleted successfully",
+          failedMessage: null
+        })
+        this.loadPlaylists()
+      })
+      .catch(err => {
+        console.log(err);
+        this.setState({
+          successMessage: null,
+          failedMessage: "Error during deleting a playlist"
+        })
+      })
+
+  }
+
+  loadPlaylists() {
+    axios.get("/playlist/index")
+      .then(res => {
+        console.log("playlists >")
+        console.log(res.data)
+        this.setState({
+          playlists:res.data
+        })
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
   render() {
     return (
       <Router>
@@ -19,7 +65,7 @@ export default class PlaylistList extends Component {
           </thead>
 
           <tbody>
-            {this.props.playlists.map((playlist, index) => (
+            {this.state.playlists.map((playlist, index) => (
               <tr key={index}>
                 <td>{index + 1}</td>
                 <td>
@@ -28,15 +74,17 @@ export default class PlaylistList extends Component {
                   </Link>
                 </td>
                 <Route
-                    path="/SongsPlaylist"
-                    component={() => (
-                      <SongsPlaylist path="/SongsPlaylist"
-                        playlistId={playlist.id}
-                      ></SongsPlaylist>
-                    )}
-                  ></Route>
+                  path="/SongsPlaylist"
+                  component={() => (
+                    <SongsPlaylist path="/SongsPlaylist"
+                      playlistId={playlist.id}
+                    ></SongsPlaylist>
+                  )}
+                ></Route>
+                <td>
+                  <Link onClick={() => this.deletePlaylist(playlist.id)} >Delete </Link>
+                </td>
               </tr>
-
             )
             )}
           </tbody>
